d3c6fe3988680a69b220d250be2d4c1e1b7f8e1a
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-10-04 23:11:00 -0300

    Real-time web e mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,43 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
-
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 
 // Acessar MongoDB Atlas
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-mlbcv.mongodb.net/proj-omn09?retryWrites=true&w=majority', {
    useNewUrlParser: true,
    useUnifiedTopology: true,
+})
+
+// Se for usar uma aplicação em produção, utilizar Redis 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+   const { user_id } = socket.handshake.query;
+
+   connectedUsers[user_id] = socket.id;
+
+   /*setTimeout(() => {
+      socket.emit('hello', 'Wolrd'); // enviando para o frontend
+   }, 4000) // demora 4s  
+   socket.on('omni', data => {
+      console.log(data);  // recebendo do frontend
+   })*/
+});
+
+app.use((req , res, next) => {
+   req.io = io;
+   req.connectedUsers = connectedUsers;
+
+   return next(); // continuar a aplicação, senão aplicação fica nessa função
 })
 
 // GET, POST, PUT, DELETE
@@ -27,4 +55,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads'))); 
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
